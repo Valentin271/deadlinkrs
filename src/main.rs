706e8523e230e89e72e9b0507b8877c021037f69
Valentin271 <valentin.doreau@free.fr>
2022-11-29 21:17:20 +0100

    basic http link search

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,16 @@ extern crate walkdir;
 
 use clap::{arg, command};
 use globset::{GlobBuilder, GlobSetBuilder};
+use regex::Regex;
+use std::fs::read_to_string;
 use walkdir::WalkDir;
 
 fn main() {
     let matches = command!()
         .arg(arg!([path]... "Path to look for files").default_value("."))
         .arg(arg!(-g --glob <glob>... "Unix-style glob to filter files").default_value("**"))
-        .arg(arg!(--hidden "Includes hidden files and directories"))
+        // .arg(arg!(--exclude <glob>... "Unix-style glob to exclude from selection"))
+        // .arg(arg!(--hidden "Includes hidden files and directories"))
         // .arg(arg!(--ignore <url>... "URL to ignore"))
         .arg(arg!(--list "List searched files and exits"))
         .get_matches();
@@ -28,6 +31,9 @@ fn main() {
 
     let globs = builder.build().unwrap();
 
+    let regex = Regex::new("(href=[\"'])?(https?://.+)[\"']?").unwrap();
+    let mut total = 0;
+
     for path in matches.get_many::<String>("path").unwrap() {
         for file in WalkDir::new(path).into_iter().filter_map(Result::ok) {
             if !file.metadata().unwrap().is_file() {
@@ -42,7 +48,19 @@ fn main() {
                 println!("{}", file.path().display());
                 continue;
             }
-            // let _content = read_to_string(file.path()).expect("File could not be red");
+
+            let content = match read_to_string(file.path()) {
+                Ok(content) => content,
+                Err(_) => continue,
+            };
+            let count = regex.find_iter(&content).count();
+
+            if count > 0 {
+                total += count;
+                println!("{} has {} link(s)", file.path().display(), count);
+            }
         }
     }
+
+    println!("{} links total", total);
 }
